4a11bed58290afaf9bbe3c8c20b4b059a32a5314
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2020-02-19 17:38:47 +0000

    SB Update with fixed H2 db version

--- src/test/java/ed/biodare2/BioDare2WSApplicationTest.java
+++ src/test/java/ed/biodare2/BioDare2WSApplicationTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
