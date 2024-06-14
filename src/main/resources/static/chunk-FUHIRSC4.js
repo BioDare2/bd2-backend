8ac32da3695f19f59bc95b4bfa35dfcbc7340b94
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2024-06-14 01:30:48 +0100

    front end update

---
+++ src/main/resources/static/chunk-FUHIRSC4.js
@@ -0,0 +1,1 @@
+import{d as e}from"./chunk-LOLEZELW.js";import{c as n}from"./chunk-FDKJ75ZE.js";import{R as r,U as a}from"./chunk-BWZCNJMV.js";var p=(()=>{let t=class t{constructor(o){this.dialog=o}show(o){this.dialog.open(e,{data:{docName:o}})}};t.\u0275fac=function(c){return new(c||t)(a(n))},t.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"});let i=t;return i})();export{p as a};
---
+++ src/main/resources/static/chunk-JESAVJXP.js
@@ -0,0 +1,1 @@
+import{T as o}from"./chunk-BWZCNJMV.js";var e=new o("param used only at tests");export{e as a};
