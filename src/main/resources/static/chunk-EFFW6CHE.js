636b1ee160af9a5f65cf7406ef00ae3dabf01021
DanielThedie <dthedie@gmail.com>
2025-07-18 15:47:36 +0100

    Accessibility improvements

--- src/main/resources/static/chunk-EFFW6CHE.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./chunk-ST3UFRAM.js";import"./chunk-EDOAR6V4.js";import"./chunk-NIE2ZYPZ.js";import"./chunk-EXZ64TUM.js";export{a as UsageModule};
---
+++ src/main/resources/static/chunk-F6KUFHEL.js
@@ -0,0 +1,1 @@
+import{l as a}from"./chunk-DECXBB7W.js";import"./chunk-EDOAR6V4.js";import"./chunk-NIE2ZYPZ.js";import"./chunk-EXZ64TUM.js";export{a as UsageModule};
